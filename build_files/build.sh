fb8fb0a20d23a94e278232e5866b22d94da2e60b
TbangIT <76477475+TbangIT@users.noreply.github.com>
2026-02-05 14:53:31 +0100

    debloat flatpak

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,7 +19,28 @@ dnf5 -y install cromite
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+FLATPAK_KILL_LIST=(
+    "org.mozilla.firefox"
+    "org.mozilla.Thunderbird"
+    "org.kde.gwenview"        # Visualizzatore Immagini
+    "org.kde.haruna"          # Video Player
+    "org.kde.okular"          # PDF Reader
+    "org.kde.kcalc"           # Calcolatrice
+    "org.kde.skanpage"        # Scanner
+    "org.kde.kontact"         # Suite contatti/mail
+    "org.kde.kweather"        # Meteo
+    "org.kde.kclock"
+    "org.gnome.DejaDup"            # Backup
+    "org.fedoraproject.MediaWriter"
+)
 
+for app in "${FLATPAK_KILL_LIST[@]}"; do
+    if [ -d "/var/lib/flatpak/app/$app" ]; then
+        rm -rf "/var/lib/flatpak/app/$app"
+        echo "Rimosso App: $app"
+    fi
+done
+flatpak uninstall --unused --system -y || true
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
